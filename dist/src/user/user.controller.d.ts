f6e2991e3ccfbb48559612620b8ab17b6afcabd3
CHANTAL KINZELMANN <chantal.kinzelmann@mercedes-benz.com>
2025-03-12 17:23:56 +0100

    recipe controller angepasst

--- dist/src/user/user.controller.d.ts
+++ dist/src/user/user.controller.d.ts
@@ -1,5 +1,11 @@
 import { UserService } from './user.service';
+import { User } from './user.entity';
 export declare class UserController {
     private readonly userService;
     constructor(userService: UserService);
+    getAllUsers(): Promise<User[]>;
+    getUser(id: number): Promise<User | null>;
+    createUser(user: User): Promise<User>;
+    updateUser(id: number, data: Partial<User>): Promise<import("typeorm").UpdateResult>;
+    deleteUser(id: number): Promise<void>;
 }
