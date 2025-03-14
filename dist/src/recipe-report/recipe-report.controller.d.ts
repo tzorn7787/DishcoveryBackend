0353b99e4077bae1d3ddeb13298f4c3ccdab2a9d
Julian Kaiser <kaiserj.tin24@student.dhbw-heidenheim.de>
2025-03-14 08:23:08 +0100

    fix_gitignore

--- dist/src/recipe-report/recipe-report.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import { RecipeReportService } from './recipe-report.service';
-export declare class RecipeReportController {
-    private readonly recipeReportService;
-    constructor(recipeReportService: RecipeReportService);
-}
--- dist/src/recipe/ingredient.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Recipe } from './recipe.entity';
-export declare class Ingredient {
-    id: number;
-    recipe: Recipe;
-    servings: string;
-    name: string;
-    amount: number;
-    unit: 'g' | 'kg' | 'ml' | 'l' | 'tbsp' | 'tsp' | 'cup' | 'piece';
-}
