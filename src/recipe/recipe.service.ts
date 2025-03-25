ba4def8b8afa3b10c001e2a7cecdc634660144cf
Timoteo Zorn <zornt.tin24@student.dhbw-heidenheim.de>
2025-03-25 14:40:39 +0100

    added other create function as comment

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -13,6 +13,56 @@ export class RecipeService {
   async create(Recipe: Recipe, userId: number): Promise<Recipe> {
     return await this.RecipesRepository.save(Recipe);
   }
+
+  // kommentiert weil keine Ahnung wie es funktioniert. mit create geht aber alles :crying-cat-face:
+  // async createRecipe(data: CreateRecipeDto, userId: number): Promise<Recipe> {
+  //   console.log('rezept kam an');
+  //   // User laden
+  //   const user = await this.userRepository.findOne({ where: { id: userId } });
+  //   if (!user) {
+  //     throw new Error('User not found');
+  //   }
+  
+  //   // Rezept-Objekt erstellen
+  //   const recipe = this.recipeRepository.create({ 
+  //     title: data.title,
+  //     description: data.description,
+  //     text: data.text,
+  //     imgUrl: data.imgUrl,
+  //     difficulty: data.difficulty,
+  //     prepTime: data.prepTime,
+  //     cookTime: data.cookTime,
+  //     user
+  //   });
+  
+  //   await this.recipeRepository.save(recipe); // 🔥 Erst speichern, damit recipe eine ID hat!
+  
+  //   // Ingredients speichern
+  //   if (data.ingredients) {
+  //     recipe.ingredients = data.ingredients.map(ingredient => {
+  //       return this.ingredientRepository.create({
+  //         name: ingredient.name,
+  //         amount: ingredient.amount,
+  //         unit: ingredient.unit as 'g' | 'kg' | 'ml' | 'l' | 'tbsp' | 'tsp' | 'cup' | 'piece',
+  //         servings: 1, // Default-Wert
+  //         recipe: { id: recipe.id } //  Nur ID übergeben
+  //       });
+  //     });
+  
+  //     await this.ingredientRepository.save(recipe.ingredients);
+  //   }
+  
+  //   // Tags speichern
+  //   if (data.tags) {
+  //     recipe.tags = await this.tagRepository.find({
+  //       where: data.tags.map(tag => ({ name: tag }))
+  //     });
+  //   }
+  
+  //   return await this.recipeRepository.save(recipe);
+  // }
+
+
   // (!) Attention: If you use this api in production, implement a "where" filter
   async readAll(): Promise<Recipe[]> {
     // return await this.RecipesRepository.find();
