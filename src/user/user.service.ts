4e3d7be8ed9b22773f8ebe8df696493f6bf08a43
CHANTAL KINZELMANN <chantal.kinzelmann@mercedes-benz.com>
2025-03-26 15:48:23 +0100

    Funktion implementiert jetzt noch stylen

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
+import { FavoriteEntry } from './favorite-entry.entity';
+import { Recipe } from 'src/recipe/recipe.entity';
+import { WatchEntry } from './watch-entry.entity';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    @InjectRepository(FavoriteEntry)
+    private favoriteRepository: Repository<FavoriteEntry>,
+    @InjectRepository(Recipe)
+    private recipeRepository: Repository<Recipe>,
+    @InjectRepository(WatchEntry)
+    private watchRepository: Repository<WatchEntry>,  
   ) {}
 
   // ✅ Benutzer erstellen mit DTO
@@ -80,4 +89,78 @@ export class UserService {
   async findByEmail(email: string) {
     return this.usersRepository.findOne({ where: { email } });
   }
+
+  async toggleFavorite(userId: number, recipeId: number): Promise<{ message: string }>{
+    const user = await this.usersRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new Error('User not found');
+    }
+  
+    const recipe = await this.recipeRepository.findOne({ where: { id: recipeId } });
+    if (!recipe) {
+      throw new Error('Rezept nicht gefunden');
+    }
+  
+    const existing = await this.favoriteRepository.findOne({
+      where: {
+        user: { id: userId },
+        recipe: { id: recipeId },
+      },
+    });
+  
+    if (existing) {
+      await this.favoriteRepository.remove(existing);
+      return { message: 'Rezept wurde aus den Favoriten entfernt.' };
+    }
+  
+    const newEntry = this.favoriteRepository.create({ user, recipe });
+    await this.favoriteRepository.save(newEntry);
+    return { message: 'Rezept wurde zu den Favoriten hinzugefügt.' };
+  }
+
+  async toggleWatch(userId: number, recipeId: number): Promise<{ message: string }> {
+    const user = await this.usersRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new Error('User not found');
+    }
+  
+    const recipe = await this.recipeRepository.findOne({ where: { id: recipeId } });
+    if (!recipe) {
+      throw new Error('Rezept nicht gefunden');
+    }
+  
+    const existing = await this.watchRepository.findOne({
+      where: {
+        user: { id: userId },
+        recipe: { id: recipeId },
+      },
+    });
+  
+    if (existing) {
+      await this.watchRepository.remove(existing);
+      return { message: 'Rezept wurde von der Merliste Favoriten entfernt.' };
+    }
+  
+    const newEntry = this.watchRepository.create({ user, recipe });
+    await this.watchRepository.save(newEntry);
+    return { message: 'Rezept wurde zur Merkliste hinzugefügt.' };
+   
+  }
+  
+  async getWatchlist(userId: number): Promise<WatchEntry[]> {
+    return this.watchRepository.find({
+      where: { user: { id: userId } },
+      relations: { recipe: true },
+    });
+  }
+  
+  async getFavorites(userId: number): Promise<FavoriteEntry[]> {
+    return this.favoriteRepository.find({
+      where: { user: { id: userId } },
+      relations: { recipe: true },
+    });
+  }
+  
+  
+  
 }
